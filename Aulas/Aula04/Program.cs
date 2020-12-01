246b2527a4e24afeb0ab342e12b7576173568239
Renanvt <jonatan_rvs@hotmail.com>
2020-12-01 00:30:20 -0300

    Escopo de variáveis

---
+++ Aulas/Aula04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula04
+{
+    class Program
+    {
+        //Variavel global
+        static int num=10; //A variável precisa ser estática para rodar no Main
+        static void Main(string[] args)
+        {
+            //Variavel local
+            int num2=0;
+            System.Console.WriteLine(num); //Exibe 10
+        }
+        void teste(){
+            int num2=1;
+            System.Console.WriteLine(num2);
+        }
+    }
+}
