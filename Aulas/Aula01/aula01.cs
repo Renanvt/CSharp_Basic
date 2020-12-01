adebcf79ecafd9d2f27105f21e665c12058093bb
Renanvt <jonatan_rvs@hotmail.com>
2020-11-30 23:52:04 -0300

    Utilizando os parametros de entrada

---
+++ Aulas/Aula01/aula01.cs
@@ -0,0 +1,11 @@
+using System; //Possui os comandos básicos de entrada e saída
+class Principal{
+    //Todo programa em si é uma classe
+    //Método principal da classe
+    static void Main(){
+        System.Console.Write("Olá Mundo...");
+        System.Console.WriteLine();
+        
+    }
+}
+
---
+++ Aulas/Aula02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*UTILIZANDO OS PARAMETROS DE ENTRADA*/
+            Console.WriteLine("Hello World!");
+            //Verificar se foi passado algum argumento no íncide do array
+            if(args.GetLength(0)>0){
+                 Console.Write(args.GetValue(0)); //Exibe o parâmetro de entrada na posição 0
+            }
+
+           
+            
+        }
+    }
+}
